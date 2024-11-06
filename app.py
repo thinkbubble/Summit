d1888424b3db673e7b73ddb78237c159930687f9
Morgan Benavidez <93624006+morganbenavidez@users.noreply.github.com>
2024-11-06 13:42:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, request
+from summit import summit
+
 
 app = Flask(__name__)
 
+
+@app.route('/summit/<path:filename>')
+def summit_static(filename):
+    return send_from_directory('summit', filename)
+
+
+
+# EDIT ANYTHING YOU WANT AFTER HERE
+
+@app.route('/documentation')
+def documentation():
+    page = request.args.get('page', 'documentation')
+    return render_template('index.html', page=page)
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    page = request.args.get('page', 'home')
+    return render_template("index.html", page=page)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
