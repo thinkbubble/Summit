47d6b13a0e12c81d06aca732fcf5646e22afb5b2
Morgan Benavidez <93624006+morganbenavidez@users.noreply.github.com>
2024-11-09 14:11:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, render_template, send_from_directory, request, jsonify
 from summit import summit
 
 
