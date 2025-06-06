c4a1bd94dff2c41743234708768938c35ac9ab2a
Morgan Benavidez <93624006+morganbenavidez@users.noreply.github.com>
2025-06-05 21:16:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 from flask import redirect, url_for
 #from dotenv import load_dotenv
 from summit import *
