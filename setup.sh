87586b9725f41ed32765b1c6e9385096ba9e0b4e
Morgan Benavidez <93624006+morganbenavidez@users.noreply.github.com>
2025-06-05 21:21:21 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,24 +7,25 @@ PROJECT_PATH="$(pwd)"
 python3 -m venv "$PROJECT_PATH"
 
 # Step 2: Set up folder structure within the project directory
-mkdir -p "$PROJECT_PATH/static/css" "$PROJECT_PATH/static/js" "$PROJECT_PATH/static/images" "$PROJECT_PATH/templates" "$PROJECT_PATH/summit"
+mkdir -p "$PROJECT_PATH/static/css" "$PROJECT_PATH/static/js" "$PROJECT_PATH/static/images" "$PROJECT_PATH/templates"
 
 # Step 3: Move provided files to the appropriate locations within the project structure
 # Check if each file exists before moving it to avoid errors
-if [[ -f "$PROJECT_PATH/main.css" ]]; then
-    mv "$PROJECT_PATH/main.css" "$PROJECT_PATH/static/css/"
+
+if [[ -f "$PROJECT_PATH/summit.css" ]]; then
+    mv "$PROJECT_PATH/summit.css" "$PROJECT_PATH/static/css/"
 fi
 
 if [[ -f "$PROJECT_PATH/summit.js" ]]; then
-    mv "$PROJECT_PATH/summit.js" "$PROJECT_PATH/summit/"
-fi
-
-if [[ -f "$PROJECT_PATH/summit.py" ]]; then
-    mv "$PROJECT_PATH/summit.py" "$PROJECT_PATH/summit/"
+    mv "$PROJECT_PATH/summit.js" "$PROJECT_PATH/static/js/"
 fi
 
 if [[ -f "$PROJECT_PATH/project.js" ]]; then
     mv "$PROJECT_PATH/project.js" "$PROJECT_PATH/static/js/"
+fi
+
+if [[ -f "$PROJECT_PATH/summit_ajax.js" ]]; then
+    mv "$PROJECT_PATH/summit_ajax.js" "$PROJECT_PATH/static/js/"
 fi
 
 if [[ -f "$PROJECT_PATH/summit.png" ]]; then
@@ -38,19 +39,15 @@ fi
 # Step 4: Activate the virtual environment and install Flask
 source "$PROJECT_PATH/bin/activate"
 pip3 install flask
-pip3 install pymongo
 pip3 install requests
 pip3 install cryptography
 pip3 install pytz
 pip3 install python-dotenv
-pip3 install pandas
-pip3 install openai
 
 source "$PROJECT_PATH/bin/activate"
 
 echo "Setup complete. Everything is set up within '$PROJECT_PATH'."
 echo "To activate the virtual environment later, use: source venv/bin/activate"
-
 
 
 # Step 5: Delete setup.sh from the folder
